1d92682de0c72f695bc4a9fdfca71dbe274e35ce
Pedroperez04 <pedroaperez021@gmail.com>
2023-04-28 04:04:07 -0300

    Agregado de Zoom al apuntar
    Agregue que cuando el personaje apunte se le haga un Zoom a la camara y solucione el problema de que la camara este un poco desviada al arrancar el juego

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Aiming : MonoBehaviour
 {
     [SerializeField] Animator m_animator;
+    public CinemachineFreeLook m_fCam;
+    private Vector3 m_v3;
+    private float m_f1;
+    public float m_normalZoom;
+    public float m_aimingZoom;
+    public float m_speedZoom;
+    public Transform m_zoomPoint;
+    public Vector3 m_vectorPoint;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        m_fCam = GetComponentInChildren<CinemachineFreeLook>();
         m_animator.GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
@@ -17,6 +28,7 @@ public class Aiming : MonoBehaviour
     {
         AimingPlayer();
         ShowCursor(false);
+       
     }
 
     private void AimingPlayer() 
@@ -24,10 +36,13 @@ public class Aiming : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             m_animator.SetBool("Aiming", true);
+            m_zoomPoint.localPosition = Vector3.SmoothDamp(m_zoomPoint.localPosition,m_vectorPoint, ref m_v3, m_speedZoom * Time.deltaTime);
+            m_fCam.m_Lens.FieldOfView = Mathf.SmoothDamp(m_fCam.m_Lens.FieldOfView, m_aimingZoom, ref m_f1, m_speedZoom * Time.deltaTime);
         }
         else
         {
             m_animator.SetBool("Aiming", false);
+            m_fCam.m_Lens.FieldOfView = Mathf.SmoothDamp(m_fCam.m_Lens.FieldOfView, m_normalZoom, ref m_f1, m_speedZoom * Time.deltaTime);
         }
     }
 
