6d90be69ed569ffa41a20db5ac1c161b4482bb7f
Pedroperez04 <pedroaperez021@gmail.com>
2023-05-02 18:25:15 -0300

    pruebas para que el enemigo ataque
    probe varias formas para que el enemigo ataque y le baje la vida al personaje pero todavia no esta del todo hecho

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    [SerializeField] Animator m_animator;
+    [SerializeField] private float m_life;
+    public EnemyAttacks m_enemyAttacks;
+    private float m_enemyAttackDamage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_enemyAttackDamage = m_enemyAttacks.m_enemyAttackDamage;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+ 
+    private void OnCollisionEnter(Collision p_collision)
+    {
+        if (p_collision.gameObject.GetComponentInParent<EnemyAttacks>())
+        {
+            EnemyAttacks l_enemyAttacks = p_collision.gameObject.GetComponentInParent<EnemyAttacks>();
+            if(l_enemyAttacks != null) 
+            {
+                TakeDamage(m_enemyAttackDamage);
+            }
+        }
+    }
+    private void TakeDamage(float p_damage)
+    {
+        m_life -= p_damage;
+        if (m_life <= 0 )
+        {
+            Debug.Log("Death");
+        }
     }
 }
